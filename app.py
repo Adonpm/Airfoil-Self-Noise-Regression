ccdf5233fb8e8ab42265189c420cd221c6952457
Adonpm <apm1@systra.com>
2025-04-22 12:30:55 +0530

    Basic structure

---
+++ app.py
@@ -0,0 +1,30 @@
+import pickle
+import pandas as pd
+import numpy as np
+from flask import Flask, request, app, jsonify
+
+app = Flask('__name__')
+
+#Loading pickle file containing the trained ML model
+model = pickle.load(open('model.pkl', 'rb'))
+
+#Creating an api
+@app.route('/predict_api', methods=['POST'])
+def predict_api():
+    data = request.json['data']
+    print(data)
+
+    # Convert the input data to a list of values
+    input_data = list(data.values())
+    
+    # Convert the input data to a 2D Array
+    input_data = [list(data.values())]
+
+    # Make a prediction using the model
+    prediction = model.predict(input_data)[0]
+    
+    # Return the prediction as a JSON response
+    return jsonify({'prediction': prediction})
+    
+if __name__ == "__main__":
+    app.run(debug=True)
