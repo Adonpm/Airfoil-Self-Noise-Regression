3ffb2c6579dca31d89663e1e9006388ed7b78e1f
Adonpm <apm1@systra.com>
2025-04-23 11:05:00 +0530

    Added routes for home page

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 import pickle
 import pandas as pd
 import numpy as np
-from flask import Flask, request, app, jsonify
+from flask import Flask, request, app, jsonify, url_for, render_template
 
 app = Flask('__name__')
 
 #Loading pickle file containing the trained ML model
 model = pickle.load(open('models/model.pkl', 'rb'))
+
+@app.route("/")
+def home():
+    #Go to home page
+    return render_template('home.html')
 
 #Creating an api
 @app.route('/predict_api', methods=['POST'])
@@ -25,6 +30,21 @@ def predict_api():
     
     # Return the prediction as a JSON response
     return jsonify({'prediction': prediction})
+
+#Creating a route for the web app
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+
+    # Convert to 2D shape
+    input_data = [data]
+
+    # Make a prediction using the model
+    prediction = model.predict(input_data)[0]
+    print(prediction)
+    
+    # Return the prediction as a JSON response
+    return render_template('home.html', prediction_text="Airfoil pressure is {}".format(prediction))
     
 if __name__ == "__main__":
     app.run(debug=True)
